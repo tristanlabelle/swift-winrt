2641949b431db2eb60754359eb24f21b08bae241
Tristan Labelle <git@tristanlabelle.com>
2023-10-21 07:46:54 -0400

    Moved getActivationFactory to own type

---
+++ Runtime/Sources/WindowsRuntime/ActivationFactory.swift
@@ -0,0 +1,14 @@
+import CABI
+import COM
+
+public enum ActivationFactory {
+    public static func getPointer<COMInterface>(activatableId: String, iid: IID) throws -> UnsafeMutablePointer<COMInterface> {
+        let activatableId = try HStringProjection.toABI(activatableId)
+        defer { HStringProjection.release(activatableId) }
+        var iid = iid
+        var factory: UnsafeMutableRawPointer?
+        try HResult.throwIfFailed(CABI.RoGetActivationFactory(activatableId, &iid, &factory))
+        guard let factory else { throw HResult.Error.noInterface }
+        return factory.bindMemory(to: COMInterface.self, capacity: 1)
+    }
+}
--- Runtime/Sources/WindowsRuntime/WinRTProjection.swift
+++ Runtime/Sources/WindowsRuntime/WinRTProjection.swift
@@ -8,15 +8,3 @@ public protocol WinRTProjection: COMProjection, IInspectableProtocol {
 
 // Protocol for strongly-typed two-way WinRT interface/delegate/runtimeclass projections into and from Swift.
 public protocol WinRTTwoWayProjection: WinRTProjection, COMTwoWayProjection {}
-
-extension WinRTProjection {
-    public static func _getActivationFactory<COMInterface>(activatableId: String, iid: IID) throws -> UnsafeMutablePointer<COMInterface> {
-        let activatableId = try HSTRING.create(Self.runtimeClassName)
-        defer { HSTRING.delete(activatableId) }
-        var iid = iid
-        var factory: UnsafeMutableRawPointer?
-        try HResult.throwIfFailed(CABI.RoGetActivationFactory(activatableId, &iid, &factory))
-        guard let factory else { throw HResult.Error.noInterface }
-        return factory.bindMemory(to: COMInterface.self, capacity: 1)
-    }
-}
