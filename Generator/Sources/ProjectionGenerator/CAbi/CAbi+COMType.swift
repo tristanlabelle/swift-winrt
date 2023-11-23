a507532df4a7d98bd8588054ec590654f8ba34f3
Tristan Labelle <git@tristanlabelle.com>
2023-11-23 06:08:34 -0500

    Write class types as their default interface in the ABI.

--- Generator/Sources/ProjectionGenerator/CAbi/CAbi+COMType.swift
+++ Generator/Sources/ProjectionGenerator/CAbi/CAbi+COMType.swift
@@ -16,13 +16,21 @@ extension CAbi {
     }
 
     internal static func toCType(_ type: TypeNode) throws -> CType {
-        guard case .bound(let type) = type else { fatalError() }
+        guard case .bound(let type) = type else { throw WinMDError.unexpectedType }
+        guard let namespace = type.definition.namespace else { throw WinMDError.unexpectedType }
 
-        if type.definition.namespace == "System" {
+        if namespace == "System" || namespace.starts(with: "System.") {
             guard let mangledName = getName(systemTypeName: type.definition.name, mangled: false) else {
                 throw WinMDError.unexpectedType
             }
             return makeCType(name: mangledName)
+        }
+
+        // At the ABI level, WinRT classes are represented as pointers to their default interface.
+        if let classDefinition = type.definition as? ClassDefinition {
+            guard !classDefinition.isStatic else { throw WinMDError.unexpectedType }
+            guard let defaultInterface = try DefaultAttribute.getDefaultInterface(classDefinition) else { throw WinMDError.missingAttribute }
+            return try toCType(defaultInterface.asNode)
         }
 
         var comType = CType.reference(name: try CAbi.mangleName(type: type))
