3e6573fb5c9b3cbb0bb8cffc3d8584bd924d012a
Tristan Labelle <git@tristanlabelle.com>
2023-11-26 07:29:51 -0500

    Fix enum RawValue comparison in test.

--- InteropTests/Tests/EnumTests.swift
+++ InteropTests/Tests/EnumTests.swift
@@ -3,14 +3,14 @@ import WinRTComponent
 
 class EnumTests: XCTestCase {
     func testNonFlags() throws {
-        XCTAssertEqual(Enum.RawValue.self, Int32.self)
+        XCTAssert(Enum.RawValue.self == Int32.self)
         XCTAssertEqual(Enum.minusOne.rawValue, -1)
         XCTAssertEqual(Enum.zero.rawValue, 0)
         XCTAssertEqual(Enum.int32Max.rawValue, Int32.max)
     }
 
     func testFlags() throws {
-        XCTAssertEqual(Flags.RawValue.self, UInt32.self)
+        XCTAssert(Flags.RawValue.self == UInt32.self)
         XCTAssertEqual(Flags.none.rawValue, 0)
         XCTAssertEqual(Flags.bit0.rawValue, 1)
         XCTAssertEqual(Flags.bit16.rawValue, 0x10000)
