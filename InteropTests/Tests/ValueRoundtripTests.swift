d00293f1359d8e7f8f08f0248a0d2d5a13ee240e
Tristan Labelle <git@tristanlabelle.com>
2023-12-11 07:26:35 -0500

    Made ValueRoundtripTests exercise two-way projections.

--- InteropTests/Tests/ValueRoundtripTests.swift
+++ InteropTests/Tests/ValueRoundtripTests.swift
@@ -3,54 +3,56 @@ import XCTest
 import WindowsRuntime
 import WinRTComponent
 
-/// Tests that values can be passed as arguments from Swift into WinRT,
-/// and returned from WinRT into Swift, without loss of information.
+/// Tests that values can be passed without loss of information
+/// between Swift and WinRT as arguments and return values.
 class ValueRoundtripTests: WinRTTestCase {
-    private var returnArgument: IReturnArgument!
+    private var oneWay: IReturnArgument!
+    private var twoWay: IReturnArgument!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
-        returnArgument = try ReturnArgument.create()
+        oneWay = ReturnArgumentImplementation()
+        twoWay = try XCTUnwrap(ReturnArgument.createProxy(oneWay))
     }
 
     override func tearDownWithError() throws {
-        returnArgument = nil
+        twoWay = nil
         try super.tearDownWithError()
     }
 
     func testInt32() throws {
-        XCTAssertEqual(try returnArgument.int32(42), 42)
+        XCTAssertEqual(try twoWay.int32(42), 42)
     }
 
     func testString() throws {
-        XCTAssertEqual(try returnArgument.string("foo"), "foo")
+        XCTAssertEqual(try twoWay.string("foo"), "foo")
     }
 
     func testObject() throws {
         let original: IInspectable = try MinimalClass()
-        let roundtripped = try XCTUnwrap(returnArgument.object(original))
+        let roundtripped = try XCTUnwrap(twoWay.object(original))
         assertCOMIdentical(original, roundtripped)
     }
 
     func testEnum() throws {
-        XCTAssertEqual(try returnArgument.enum(.one), .one)
+        XCTAssertEqual(try twoWay.enum(.one), .one)
     }
 
     func testStruct() throws {
         let original = MinimalStruct(field: 42)
-        XCTAssertEqual(try returnArgument.struct(original), original)
+        XCTAssertEqual(try twoWay.struct(original), original)
     }
 
     func testInterface() throws {
         let original: IMinimalInterface = try MinimalClass()
-        let roundtripped = try XCTUnwrap(returnArgument.interface(original))
+        let roundtripped = try XCTUnwrap(twoWay.interface(original))
         assertCOMIdentical(original, roundtripped)
     }
 
     func testClass() throws {
         let instance = try MinimalClass()
         XCTAssertEqual(
-            try XCTUnwrap(returnArgument.class(instance)).comPointer,
+            try XCTUnwrap(twoWay.class(instance)).comPointer,
             instance.comPointer)
     }
 
@@ -58,13 +60,29 @@ class ValueRoundtripTests: WinRTTestCase {
         // By design, Swift does not support identity comparisons for closures,
         // so we can only test that the round-tripped closure is still calling the same code.
         var called = false
-        let result = try XCTUnwrap(returnArgument.delegate({ called = true }))
+        let result = try XCTUnwrap(twoWay.delegate({ called = true }))
         XCTAssertFalse(called)
         try result()
         XCTAssertTrue(called)
     }
 
-    func testArray() throws {
-        XCTAssertEqual(try returnArgument.array(["a", "b"]), ["a", "b"])
+    func testArray_oneWay() throws {
+        XCTAssertEqual(try oneWay.array(["a", "b"]), ["a", "b"])
+    }
+
+    func testArray_twoWay() throws {
+        throw XCTSkip("Two-way array projections are not implemented yet")
+    }
+
+    class ReturnArgumentImplementation: WinRTExportBase<IReturnArgumentProjection>, IReturnArgumentProtocol {
+        func int32(_ value: Int32) throws -> Int32 { value }
+        func string(_ value: String) throws -> String { value }
+        func object(_ value: WindowsRuntime.IInspectable?) throws -> WindowsRuntime.IInspectable? { value }
+        func `enum`(_ value: WinRTComponent.MinimalEnum) throws -> WinRTComponent.MinimalEnum { value }
+        func `struct`(_ value: WinRTComponent.MinimalStruct) throws -> WinRTComponent.MinimalStruct { value }
+        func interface(_ value: WinRTComponent.IMinimalInterface?) throws -> WinRTComponent.IMinimalInterface? { value }
+        func `class`(_ value: WinRTComponent.MinimalClass?) throws -> WinRTComponent.MinimalClass? { value }
+        func delegate(_ value: WinRTComponent.MinimalDelegate?) throws -> WinRTComponent.MinimalDelegate? { value }
+        func array(_ value: [String]) throws -> [String] { value }
     }
 }
