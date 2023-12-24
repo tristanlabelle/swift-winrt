fedfa4d9996df57d7e5aeec2c107d95649aa377f
Tristan Labelle <git@tristanlabelle.com>
2023-12-24 08:25:32 -0500

    Tweaks

--- InteropTests/Tests/WinRTTestCase.swift
+++ InteropTests/Tests/WinRTTestCase.swift
@@ -4,9 +4,14 @@ import WindowsRuntime
 class WinRTTestCase: XCTestCase {
     // Only created once before the first test of any test case runs,
     // and never freed because we don't know when we've run the last test,
-    // and reinitializing WinRT could invalidated cached activation factories.
-    private static var initialization: Result<WinRTInitialization, any Error>
+    // and reinitializing WinRT could invalidate cached activation factories.
+    private static let initialization: Result<WinRTInitialization, any Error>
         = Result { try WinRTInitialization(multithreaded: true) }
+
+    override class func setUp() {
+        // Enforce that the static initializer runs before any tests.
+        _ = try? initialization.get()
+    }
 
     override func setUpWithError() throws {
         _ = try Self.initialization.get()
