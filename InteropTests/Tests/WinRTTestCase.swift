743b9c3fa1d55c7fc5641fe6058f57c121bc33e4
Tristan Labelle <git@tristanlabelle.com>
2023-12-24 08:16:02 -0500

    Avoid reinitializing WinRT in tests.

--- InteropTests/Tests/WinRTTestCase.swift
+++ InteropTests/Tests/WinRTTestCase.swift
@@ -2,19 +2,13 @@ import XCTest
 import WindowsRuntime
 
 class WinRTTestCase: XCTestCase {
-    internal class var singleThreadedApartment: Bool { false }
-
-    private static var initialization: Result<WinRTInitialization, Error>?
-
-    override class func setUp() {
-        initialization = Result { try WinRTInitialization(multithreaded: !singleThreadedApartment) }
-    }
+    // Only created once before the first test of any test case runs,
+    // and never freed because we don't know when we've run the last test,
+    // and reinitializing WinRT could invalidated cached activation factories.
+    private static var initialization: Result<WinRTInitialization, any Error>
+        = Result { try WinRTInitialization(multithreaded: true) }
 
     override func setUpWithError() throws {
-        _ = try XCTUnwrap(Self.initialization).get()
-    }
-
-    override class func tearDown() {
-        initialization = nil
+        _ = try Self.initialization.get()
     }
 }
