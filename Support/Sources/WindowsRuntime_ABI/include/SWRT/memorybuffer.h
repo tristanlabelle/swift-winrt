d77653cfdfe3d438c323d0c0f2a673f6e3161fd6
Tristan Labelle <git@tristanlabelle.com>
2024-05-01 08:13:25 -0400

    Added IMemoryBufferByteAccess (#151)

---
+++ Support/Sources/WindowsRuntime_ABI/include/SWRT/memorybuffer.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "SWRT/unknwn.h"
+
+typedef struct SWRT_IMemoryBufferByteAccess SWRT_IMemoryBufferByteAccess;
+
+struct SWRT_IMemoryBufferByteAccess_VirtualTable {
+    SWRT_HResult (__stdcall* QueryInterface)(SWRT_IMemoryBufferByteAccess* _this, SWRT_Guid* riid, void** ppvObject);
+    uint32_t (__stdcall* AddRef)(SWRT_IMemoryBufferByteAccess* _this);
+    uint32_t (__stdcall* Release)(SWRT_IMemoryBufferByteAccess* _this);
+    SWRT_HResult (__stdcall* GetBuffer)(SWRT_IMemoryBufferByteAccess* _this, uint8_t** value, uint32_t* capacity);
+};
+
+struct SWRT_IMemoryBufferByteAccess {
+    struct SWRT_IMemoryBufferByteAccess_VirtualTable* VirtualTable;
+};
