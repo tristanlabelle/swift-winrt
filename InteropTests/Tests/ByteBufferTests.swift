e656b0b1e28538c59a284c7ceaf71889c90eee59
Tristan Labelle <git@tristanlabelle.com>
2024-06-23 16:52:51 -0400

    Add tests for IMemoryBuffer and IBuffer interop

---
+++ InteropTests/Tests/ByteBufferTests.swift
@@ -0,0 +1,39 @@
+import UWP_WindowsFoundation
+import UWP_WindowsStorageStreams
+import WindowsRuntime
+import WinRTComponent
+
+class ByteBufferTests : XCTestCase {
+    public func testConsumeMemoryBuffer() throws {
+        let bytes: [UInt8] = [1, 2, 3]
+        let memoryBuffer = try ByteBuffers.arrayToMemoryBuffer(bytes)
+        let memoryBufferReference = try memoryBuffer.createReference()
+        let roundtrippedBytes = Array(UnsafeBufferPointer(try memoryBufferReference.bytes, try memoryBuffer._length()))
+        XCTAssertEqual(roundtrippedBytes, bytes)
+    }
+
+    public func testConsumeStorageBuffer() throws {
+        var bytes: [UInt8] = [1, 2, 3]
+        var storageBuffer = try ByteBuffers.arrayToStorageBuffer(bytes)
+        let roundtrippedBytes = Array(storageBuffer.bytes)
+        XCTAssertEqual(roundtrippedBytes, bytes)
+    }
+
+    public func testProduceMemoryBuffer() throws {
+        var bytes: [UInt8] = [1, 2, 3]
+        let memoryBuffer = try MemoryBuffer(UInt32(bytes.count))
+        let memoryBufferReference = try memoryBuffer.createReference()
+        let unsafeBuffer = UnsafeMutableBufferPointer(try memoryBufferReference.bytes, try memoryBuffer._length())
+        unsafeBuffer.update(fromContentsOf: bytes)
+        var roundtrippedBytes = try ByteBuffers.memoryBufferToArray(memoryBuffer)
+        XCTAssertEqual(roundtrippedBytes, bytes)
+    }
+
+    public func testProduceStorageBuffer() throws {
+        var bytes: [UInt8] = [1, 2, 3]
+        let buffer = try Buffer(UInt32(bytes.count))
+        try buffer.bytes.update(fromContentsOf: bytes)
+        var roundtrippedBytes = try ByteBuffers.storageBufferToArray(buffer)
+        XCTAssertEqual(roundtrippedBytes, bytes)
+    }
+}
