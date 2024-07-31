5ece6f9408230fe6835688e296be4e34d6b20b27
Tristan Labelle <git@tristanlabelle.com>
2024-07-31 09:10:42 -0400

    Tweak EventRegistration + codegen for allocation-less usage (#231)

--- Support/Sources/WindowsRuntime/EventRegistration.swift
+++ Support/Sources/WindowsRuntime/EventRegistration.swift
@@ -1,25 +1,32 @@
+/// Represents a registration of an event handler with an event source and supports removing the registration.
 public struct EventRegistration {
-    public typealias Remover = (_ removing: EventRegistrationToken) throws -> Void
+    // We store and pass in the event source object so removal closures do not need to capture context, avoiding allocations.
+    public typealias Remover = (_ source: AnyObject, _ token: EventRegistrationToken) throws -> Void
 
+    /// The object that is the source of the event.
+    private var source: AnyObject
+    /// The token that represents the event registration.
     public private(set) var token: EventRegistrationToken
-    public private(set) var remover: Remover
+    /// The closure that removes the event registration.
+    private var remover: Remover
 
-    public init(token: EventRegistrationToken, remover: @escaping Remover) {
+    public init(source: AnyObject, token: EventRegistrationToken, remover: @escaping Remover) {
+        self.source = source
         self.token = token
         self.remover = remover
     }
 
     public mutating func detachToken() -> EventRegistrationToken {
-        defer { token = .none }
         remover = Self.nullRemover
+        defer { token = .none }
         return token
     }
 
     public mutating func remove() throws {
-        try remover(token)
+        try remover(source, token)
         token = .none
         remover = Self.nullRemover
     }
 
-    private static let nullRemover: Remover = { _ in }
+    private static let nullRemover: Remover = { _, _ in }
 }
