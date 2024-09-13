a92750768df39362f44aba4a97402222f64a7570
Tristan Labelle <git@tristanlabelle.com>
2024-09-13 06:57:59 -0400

    Fix incorrect CMakeLists.txt codegen (#282)
    Some keywords must be UPPERCASE.

--- Generator/Sources/CodeWriters/CMake/CMakeListsWriter.swift
+++ Generator/Sources/CodeWriters/CMake/CMakeListsWriter.swift
@@ -6,7 +6,7 @@ public final class CMakeListsWriter {
     }
 
     public func writeAddLibrary(_ name: String, _ type: CMakeLibraryType? = nil, _ sources: [String] = []) {
-        let typeSuffix = type.map { " \($0)" } ?? ""
+        let typeSuffix = type.map { " \($0.rawValue)" } ?? ""
         output.writeIndentedBlock(grouping: .never, header: "add_library(\(name)\(typeSuffix)", footer: ")") {
             for source in sources {
                 output.writeFullLine(source)
@@ -16,7 +16,7 @@ public final class CMakeListsWriter {
 
     public func writeTargetIncludeDirectories(_ target: String, _ visibility: CMakeVisibility, _ directories: [String]) {
         guard !directories.isEmpty else { return }
-        output.writeIndentedBlock(grouping: .never, header: "target_include_directories(\(target) \(visibility)", footer: ")") {
+        output.writeIndentedBlock(grouping: .never, header: "target_include_directories(\(target) \(visibility.rawValue)", footer: ")") {
             for directory in directories {
                 output.writeFullLine(directory)
             }
@@ -25,14 +25,14 @@ public final class CMakeListsWriter {
 
     public func writeTargetLinkLibraries(_ target: String, _ visibility: CMakeVisibility, _ libraries: [String]) {
         guard !libraries.isEmpty else { return }
-        output.writeIndentedBlock(grouping: .never, header: "target_link_libraries(\(target) \(visibility)", footer: ")") {
+        output.writeIndentedBlock(grouping: .never, header: "target_link_libraries(\(target) \(visibility.rawValue)", footer: ")") {
             for library in libraries {
                 output.writeFullLine(library)
             }
         }
     }
 
-    public func writeAddSubdirectory(_ subdirectory: String) {
-        output.writeFullLine(grouping: .withName("add_subdirectory"), "add_subdirectory(\(subdirectory))")
+    public func writeAddSubdirectory(_ sources: String) {
+        output.writeFullLine(grouping: .withName("add_subdirectory"), "add_subdirectory(\(sources))")
     }
 }
