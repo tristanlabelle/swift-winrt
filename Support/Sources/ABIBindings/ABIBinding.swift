f9a57704bc73f88e622190443c528b0afa2cdeb5
Tristan Labelle <git@tristanlabelle.com>
2024-09-16 18:36:36 -0400

    Extract an ABIBindings module.

--- Support/Sources/ABIBindings/ABIBinding.swift
+++ Support/Sources/ABIBindings/ABIBinding.swift
@@ -40,13 +40,6 @@ extension ABIBinding {
     }
 }
 
-public protocol IdentityBinding: PODBinding where SwiftValue == ABIValue {}
-
-extension IdentityBinding {
-    public static func toABI(_ value: SwiftValue) -> ABIValue { value }
-    public static func toSwift(_ value: ABIValue) -> SwiftValue { value }
-}
-
 public enum ABIBindingError: Error {
     case unsupported(Any.Type)
 }
---
+++ Support/Sources/ABIBindings/ClosedEnumBinding.swift
@@ -0,0 +1,11 @@
+/// Binding for an enumeration whose ABI representation is an integer,
+/// and for which allowed values are limited to a set of defined enumerants.
+public protocol ClosedEnumBinding: RawRepresentable, PODBinding 
+    where ABIValue == RawValue, SwiftValue == Self, RawValue: FixedWidthInteger & Hashable {
+}
+
+extension ClosedEnumBinding {
+    public static var abiDefaultValue: RawValue { RawValue.zero }
+    public static func toSwift(_ value: RawValue) -> Self { Self(rawValue: value)! }
+    public static func toABI(_ value: Self) -> RawValue { value.rawValue }
+}
