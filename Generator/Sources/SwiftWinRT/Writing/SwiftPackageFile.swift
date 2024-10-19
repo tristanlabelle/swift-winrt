3fa1e51fdab66b4500c0119e02b3f7da7da189e6
Tristan Labelle <git@tristanlabelle.com>
2024-10-18 23:13:02 -0400

    Remove -wmo with SPM

--- Generator/Sources/SwiftWinRT/Writing/SwiftPackageFile.swift
+++ Generator/Sources/SwiftWinRT/Writing/SwiftPackageFile.swift
@@ -20,7 +20,7 @@ func writeSwiftPackageFile(_ projection: Projection, spmOptions: SPMOptions, toP
         var projectionModuleTarget: SwiftPackage.Target = .target(name: module.name)
         projectionModuleTarget.path = "\(module.name)/Projection"
         projectionModuleTarget.dependencies.append(.product(name: "WindowsRuntime", package: "Support"))
-        projectionModuleTarget.swiftUnsafeFlags.append("-whole-module-optimization") // See equivalent CMake-writing code
+        // Cannot add -whole-module-optimization like the CMake code because SPM already adds -enable-batch-mode
 
         for referencedModule in module.references {
             guard !referencedModule.isEmpty else { continue }
