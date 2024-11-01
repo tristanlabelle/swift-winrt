99c8905109d5f09ea40430286e503c2478695466
Tristan Labelle <git@tristanlabelle.com>
2024-11-01 14:06:45 -0400

    Support dynamic instantiation of derived activatable classes

--- Support/Sources/WindowsRuntime/BindingProtocols+extensions.swift
+++ Support/Sources/WindowsRuntime/BindingProtocols+extensions.swift
@@ -67,7 +67,11 @@ extension DelegateBinding {
     }
 }
 
-extension ActivatableClassBinding {
+extension RuntimeClassBinding {
+    public static func _wrapObject(_ reference: consuming IInspectableReference) throws -> IInspectable {
+        try _wrap(reference.queryInterface(interfaceID)) as! IInspectable
+    }
+
     // Shadow COMTwoWayBinding methods to use WinRTError instead of COMError
     public static func _implement<This>(_ this: UnsafeMutablePointer<This>?, _ body: (SwiftObject) throws -> Void) -> SWRT_HResult {
         guard let this else { return WinRTError.toABI(hresult: HResult.pointer, message: "WinRT 'this' pointer was null") }
@@ -88,28 +92,10 @@ extension ComposableClassBinding {
         guard let pointer = value else { return nil }
         let reference = COMReference(transferringRef: pointer)
         if let swiftObject = _unwrap(reference.pointer) { return swiftObject }
-        return swiftWrapperFactory.create(reference, binding: Self.self)
+        return swiftWrapperFactory.create(reference, staticBinding: Self.self)
     }
 
     public static func _unwrap(_ pointer: ABIPointer) -> SwiftObject? {
         COMEmbedding.getImplementation(pointer, type: SwiftObject.self)
-    }
-
-    public static func _wrapObject(_ reference: consuming IInspectableReference) -> IInspectable {
-        try! _wrap(reference.queryInterface(interfaceID)) as! IInspectable
-    }
-
-    // Shadow COMTwoWayBinding methods to use WinRTError instead of COMError
-    public static func _implement<This>(_ this: UnsafeMutablePointer<This>?, _ body: (SwiftObject) throws -> Void) -> SWRT_HResult {
-        guard let this else { return WinRTError.toABI(hresult: HResult.pointer, message: "WinRT 'this' pointer was null") }
-        let implementation: SwiftObject = COMEmbedding.getImplementationOrCrash(this)
-        return WinRTError.toABI { try body(implementation) }
-    }
-
-    public static func _getter<Value>(_ this: ABIPointer?, _ value: UnsafeMutablePointer<Value>?, _ code: (SwiftObject) throws -> Value) -> SWRT_HResult {
-        _implement(this) {
-            guard let value else { throw COMError.pointer }
-            value.pointee = try code($0)
-        }
     }
 }
