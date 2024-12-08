4ed771f68a13a67cd51c4cbea80f231120bc4fb7
Tristan Labelle <git@tristanlabelle.com>
2024-12-08 08:50:23 -0500

    Fix test errors

--- InteropTests/Tests/FlatModuleTests.swift
+++ InteropTests/Tests/FlatModuleTests.swift
@@ -2,5 +2,5 @@ import UWP_Flat
 
 // Compile-time tests that the UWP_Flat flat module
 // exposes short names for types in that module.
-typealias MemoryBufferExists = MemoryBuffer // From UWP_WindowsFoundation
-typealias IBufferExists = IBuffer // From UWP_WindowsStorageStreams
+fileprivate typealias MemoryBufferExists = MemoryBuffer // From UWP_WindowsFoundation
+fileprivate typealias IBufferExists = IBuffer // From UWP_WindowsStorageStreams
--- InteropTests/Tests/NamespaceModuleTests.swift
+++ InteropTests/Tests/NamespaceModuleTests.swift
@@ -2,4 +2,4 @@ import UWP_WindowsFoundation
 
 // Compile-time tests that the UWP_WindowsFoundation namespace module
 // exposes short names for types in that namespace.
-typealias MemoryBufferExists = MemoryBuffer
+fileprivate typealias MemoryBufferExists = MemoryBuffer
